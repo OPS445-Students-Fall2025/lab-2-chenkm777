8fb39c5a86316bf508c136dcace76e6ee6f68143
kaiming chen <kchen129@myseneca.ca>
2025-09-18 23:00:42 -0400

    Individual message or note.

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Kaiming Chen
+# Author ID: kchen129
+# Created: 2025/09/18 
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
+
